4a34e8406237edbcdda1835d80689f44f7770070
josue-lubaki <josuelubaki@gmail.com>
2021-02-14 18:15:31 -0500

    creation du DAO

---
+++ app/src/main/java/ca/josue/codelab/dao/WordDao.java
@@ -0,0 +1,37 @@
+package ca.josue.codelab.dao;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
+import ca.josue.codelab.model.Word;
+
+@Dao
+public interface WordDao {
+    /*
+        Qu'est-ce que le DAO?
+            Un DAO (objet d'accès aux données) valide votre SQL au moment de la compilation et l'associe à une méthode.
+            Dans votre salle DAO, vous utilisez des annotations pratiques, comme @Insert,
+            pour représenter les opérations de base de données les plus courantes!
+            Room utilise le DAO pour créer une API propre pour votre code.
+            Le DAO doit être une interface ou une classe abstraite.
+            Par défaut, toutes les requêtes doivent être exécutées sur un thread distinct.
+    */
+
+    // Obtenir tous les mots classés par ordre alphabétique
+    @Query("SELECT * FROM word_table ORDER BY word ASC")
+    List<Word> getAlphabetizedWords();
+
+    @Query("DELETE FROM word_table")
+    void deleteAll();
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Word word);
+
+
+
+
+}
--- app/src/main/java/ca/josue/codelab/model/Word.java
+++ app/src/main/java/ca/josue/codelab/model/Word.java
@@ -6,7 +6,7 @@ import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 @Entity(tableName = "word_table")
-public class word {
+public class Word {
     @PrimaryKey(autoGenerate = true)
     private int id;
 
@@ -14,7 +14,7 @@ public class word {
     @ColumnInfo(name = "word")
     private final String mWord;
 
-    public word(@NonNull String word) {
+    public Word(@NonNull String word) {
         this.mWord = word;
     }
 
